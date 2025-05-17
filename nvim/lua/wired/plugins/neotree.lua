a0e294b852e80756ec84a6c6af9af1a79557fa35
E-lmir <mollymaloun@gmail.com>
2025-05-17 19:44:47 +0400

    update neo tree

--- nvim/lua/wired/plugins/neotree.lua
+++ nvim/lua/wired/plugins/neotree.lua
@@ -1,6 +1,17 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    config = function()
+	    require("neo-tree").setup({
+    		enable_git_status = true,
+    		close_if_last_window = true,
+    		filesystem = {
+	   		 follow_current_file = {
+				    enabled = true
+	 		   }
+   		 }
+	 })
+    end,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
